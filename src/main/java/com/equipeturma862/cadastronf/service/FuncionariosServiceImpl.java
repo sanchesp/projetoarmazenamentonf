3cd2628f9bf5a65f6fbad112b7e85354cfe80067
Marcos <marcossanches1@hotmail.com.br>
2022-10-27 10:53:31 -0300

    implemento da classe FuncionarioServiceImpl na packge service

--- src/main/java/com/equipeturma862/cadastronf/service/FuncionariosServiceImpl.java
+++ src/main/java/com/equipeturma862/cadastronf/service/FuncionariosServiceImpl.java
@@ -3,13 +3,42 @@ package com.equipeturma862.cadastronf.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class FuncionariosServiceImpl implements FuncionariosService{
 
     private final CadastroNFRepository cadastroNFRepository;
 
+    @Override
+    public List<Funcionarios> list() {
+        return IterableUtils.toList(cadastroNFRepository.findAll());
+    }
 
+
+    @Override
+    public Funcionarios save(Funcionarios complexoEolico) {
+        return cadastroNFRepository.save(complexoEolico);
+    }
+
+    @Override
+    public Funcionarios getById(Long id) {
+        return cadastroNFRepository.findById(id).get();
+
+    }
+
+    @Override
+    public Funcionarios update(Long id, Funcionarios complexoEolico) {
+        Funcionarios complexoEolicoUpdate = cadastroNFRepository.findById(id).get();
+        complexoEolicoUpdate.setNome(complexoEolicoUpdate.getNome());
+        return cadastroNFRepository.save(complexoEolicoUpdate);
+    }
+
+    @Override
+    public void delete(Long id) {
+        cadastroNFRepository.deleteById(id);
+    }
 
 
 }
