0494fd82f6e5cde19ff5b2ef7d8144b338a86668
Marcos <marcossanches1@hotmail.com.br>
2022-10-27 10:31:22 -0300

    implemento da classe FuncionarioServiceImpl na packge service

---
+++ src/main/java/com/equipeturma862/cadastronf/service/FuncionariosServiceImpl.java
@@ -0,0 +1,15 @@
+package com.equipeturma862.cadastronf.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FuncionariosServiceImpl implements FuncionariosService{
+
+    private final CadastroNFRepository cadastroNFRepository;
+
+
+
+
+}
